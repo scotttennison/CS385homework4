edb53b89c903184ee0d5a0ce7970996315ed22f0
Scott Tennison <scotttennison@Scotts-14-Incher.local>
2026-03-30 11:46:30 -0700

    Fix app deployment using systemd service

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,13 +20,6 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
@@ -36,23 +29,41 @@ resource "aws_security_group" "app_sg" {
 }
 
 resource "aws_instance" "app_server" {
-  ami = "ami-0ceecbb0f30a902a6"
+  ami           = "ami-0ceecbb0f30a902a6"
   instance_type = "t2.micro"
 
   vpc_security_group_ids = [aws_security_group.app_sg.id]
 
-user_data = <<-EOF
-  #!/bin/bash
-  yum update -y
-  yum install -y python3 python3-pip git
-  pip3 install flask gunicorn
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    yum install -y python3 python3-pip git
 
-  cd /home/ec2-user
-  git clone https://github.com/scotttennison/CS385homework4.git app
-  cd app
+    pip3 install flask gunicorn
 
-  gunicorn --bind 0.0.0.0:5000 --daemon app:app
-EOF
+    cd /home/ec2-user
+    git clone https://github.com/scotttennison/CS385homework4.git app
+    cd app
+
+    cat > /etc/systemd/system/flaskapp.service <<SERVICE
+    [Unit]
+    Description=Flask App
+    After=network.target
+
+    [Service]
+    User=ec2-user
+    WorkingDirectory=/home/ec2-user/app
+    ExecStart=/usr/local/bin/gunicorn --bind 0.0.0.0:5000 app:app
+    Restart=always
+
+    [Install]
+    WantedBy=multi-user.target
+    SERVICE
+
+    systemctl daemon-reload
+    systemctl enable flaskapp
+    systemctl start flaskapp
+  EOF
 
   tags = {
     Name = "CS385-App-Server"
